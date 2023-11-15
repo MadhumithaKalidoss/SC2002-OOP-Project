a6a088fbba31d5ac8d28a259bdcf955e17e35501
senchiagladine <129827870+senchiagladine@users.noreply.github.com>
2023-11-15 21:43:24 +0800

    Add files via upload

---
+++ CampAvailabilityService.java
@@ -0,0 +1,15 @@
+package oopproject;
+
+public class CampAvailabilityService {
+
+	public void isFull() {
+		// TODO - implement CampAvailabiltyService.isFull
+		throw new UnsupportedOperationException();
+	}
+
+	public void isOpen() {
+		// TODO - implement CampAvailabiltyService.isOpen
+		throw new UnsupportedOperationException();
+	}
+
+}
