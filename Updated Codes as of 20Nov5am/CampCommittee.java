af48b5cc71d86bd1a66a41763084ccd76bb3f391
MadhumithaKalidoss <131425403+MadhumithaKalidoss@users.noreply.github.com>
2023-12-04 00:02:56 +0800

    Delete Updated Codes as of 20Nov5am directory

--- Updated Codes as of 20Nov5am/CampCommittee.java
+++
@@ -1,8 +0,0 @@
-public class CampCommittee extends Student{
-
-	public CampCommittee(String name, String email, String faculty) {
-		super(name, email, faculty);
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Updated Codes as of 20Nov5am/Staff.java
+++
@@ -1,8 +0,0 @@
-public class Staff extends User{
-
-	public Staff(String name, String email, String faculty) {
-		super(name, email, faculty);
-		// TODO Auto-generated constructor stub
-	}
-
-}
